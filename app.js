c103117e20679ea7b83579bf0f05ff271a9770b7
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-01-05 15:23:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// let age = prompt("enter your age");
+
+// if (age >= 18) {
+//   console.log("You are able to drive");
+// } else console.log("You are not able to drive");
+
+// let lightColor = prompt("Pick one color: 1.Red 2.Yellow 3.Green");
+
+// if (lightColor === "green") {
+//   console.log("Go...", lightColor);
+// } else if (lightColor === "red") {
+//   console.log("Stop!", lightColor);
+// } else if (lightColor === "yellow") {
+//   console.log("Wait!!", lightColor);
+// } else {
+//   console.log("Invalid color");
+
+// }
+
+// let popcornPrice = prompt("Enter price");
+
+// if(popcornPrice === "250"){
+//   console.log("size is XL")
+// }else if(popcornPrice === "200"){
+//   console.log("Size is L")
+// }else if(popcornPrice === "150"){
+//   console.log("Size is M")
+// }else if(popcornPrice === "100"){
+//   console.log("Size is MS")
+// }else if(popcornPrice === "50"){
+//   console.log("Size is S")
+// }else{console.log("Invalid Value")}
+
+// let size = prompt("Enter the size do you want? xl, l, m,s");
+
+// if (size === "xl") {
+//   console.log("The Price of Popcon is 250");
+// } else if (size === "l") {
+//   console.log("The Price of Popcon is 200");
+// } else if (size === "m") {
+//   console.log("The Price of Popcon is 100");
+// } else if (size === "s") {
+//   console.log("The Price of Popcon is 50");
+// } else {
+//   console.log("Invalid Value");
+// }
+
+let marks = prompt("Enter Your Marks");
+
+if (marks >= "33") {
+  console.log("Pass");
+  if (marks >= "90") {
+    console.log("Good You Got A+ Grade");
+  } else if (marks >= "80") {
+    console.log("Good You Got A Grade");
+  } else if (marks >= "70") {
+    console.log("Good You Got B Grade");
+  } else if (marks >= "60") {
+    console.log("Good You Got C Grade");
+  } else if (marks >= "50") {
+    console.log("Good You Got D Grade");
+  }
+} else console.log("Better Luck Next Time");
