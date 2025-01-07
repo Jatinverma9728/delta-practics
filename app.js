ea379138719c63b14fa9f5f66564321a1fb537f1
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-01-07 16:04:17 +0530

    day 7

--- app.js
+++ app.js
@@ -66,8 +66,121 @@
 
 // --------------------Logical Operator-------------------
 
-let str = "apple";
+// let str = "apple";
 
-if (str.length >= 3 && str[0] === "a") {
-  console.log("Good string");
-} else console.log("bad string");
+// if (str.length >= 3 && str[0] === "a") {
+//   console.log("Good string");
+// } else console.log("bad string");
+
+// -------------Switch statement--------------
+
+// let color = "red";
+
+// switch(color){
+//   case "red" :
+//     console.log("Stop here!!")
+//   break;
+//   case "yellow" :
+//     console.log("Slow down")
+//   break;
+//   case "green" :
+//     console.log("Go")
+//   break;
+//   default:console.log("light is broken")
+// }
+
+// let day = prompt("enter a number");
+
+// switch(day){
+//   case "1":
+//     console.log("Monday");
+//     break;
+//   case "2":
+//     console.log("Tuesday");
+//     break;
+//   case "3":
+//     console.log("Wednesday");
+//     break;
+//   case "4":
+//     console.log("Thursday");
+//     break;
+//   case "5":
+//     console.log("Friday");
+//     break;
+//   case "6":
+//     console.log("Saturday");
+//     break;
+//   case "7":
+//     console.log("Sunday");
+//     break;
+//     default:console.log("Invalid number")
+// }
+
+// let num = prompt("Enter any nummber");
+
+// if (num % 10 === 0) {
+//   console.log("Its a good number");
+// } else {
+//   console.log("its a bad number");
+// }
+
+// let userName = prompt("Enter your name");
+// let userAge = prompt("Enter your age");
+// // let fullName = console.log(`Your name ${userName} and your age is ${userAge}`)
+
+// alert(`Your name ${userName} and your age is ${userAge}`);
+
+// let quater = prompt("Enter a number");
+
+// switch(quater){
+//   case "1":
+//     console.log("January","Febuary","March")
+//     break;
+//   case "2":
+//     console.log("April","May","June")
+//     break;
+//   case "3":
+//     console.log("July","August","September")
+//     break;
+//   case "4":
+//     console.log("Otcober","November","December")
+//     break;
+//     default:console.log("Wrong Input")
+
+// }
+
+// let str = "Apple";
+
+// if(str.length >=5 && str[0]==="a" || str[0] === "A"){
+// console.log("Golden string")
+// }else{
+//   console.log("Not a Golden string")
+// }
+
+// let a = 1;
+// let b = 8;
+// let c = 9;
+
+// if (a > b) {
+//   if (a > c) {
+//     console.log(a, "is largest");
+//   } else {
+//     console.log(c, "is largest");
+//   }
+// } else {
+//   if (b > c) {
+//     console.log(b, "is largest");
+//   } else {
+//     console.log(c, "is largest");
+//   }
+// }
+
+
+
+let num1 = 32;
+let num2 = 4785444444444442;
+
+
+if((num1%10)==(num2%10)){
+  console.log("same digit",num1%10)
+}else(console.log("not have same number"))
