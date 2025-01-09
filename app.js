137a7030d2176dc63cafbade3de556530c5ed6b4
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-01-10 00:14:14 +0530

    day 8

--- app.js
+++ app.js
@@ -175,12 +175,38 @@
 //   }
 // }
 
+// let num1 = 32;
+// let num2 = 4785444444444442;
+
+// if((num1%10)==(num2%10)){
+//   console.log("same digit",num1%10)
+// }else(console.log("not have same number"))
 
 
-let num1 = 32;
-let num2 = 4785444444444442;
+// let str = "ilovejavascript"
+// console.log(str.slice(1,5)); // give the limited outputwhat we write in index of string 
+// console.log(str.slice(0,str.length-0)); // give complete string length-0  
+// console.log(str.slice(-6)); // that is puri length me se minus krege kitna kre ga jitni hum value denge yha -1  kre ga matlab ab ye last element ko print kre ga 
+
+// console.log(str.replace("love", "enjoy")) // replace words
+// console.log(str.replace("o", "x")); // replace only first matching character
+// console.log(str.replaceAll("a","x")) // replace all matching character
+
+// let fruit = "mango";
+
+// console.log(fruit.repeat(5))
 
 
-if((num1%10)==(num2%10)){
-  console.log("same digit",num1%10)
-}else(console.log("not have same number"))
+
+// ----------------QS-------
+
+let msg = "    help!";
+let newMsg = msg.trim();
+console.log(newMsg.toUpperCase())
+// console.log(msg.trim(),msg.toUpperCase())
+
+let name = "ApnaCollege";
+console.log(name.slice(4,9), name.indexOf("na"), name.replace("Apna","Our"));
+
+let newName = name.slice(4)
+console.log(newName.replaceAll("l","t"))
