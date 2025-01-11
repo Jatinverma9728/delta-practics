6bdf9faa053a477faac9884749b6a34c21ac3654
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-01-11 17:37:44 +0530

    day 9

--- app.js
+++ app.js
@@ -182,11 +182,10 @@
 //   console.log("same digit",num1%10)
 // }else(console.log("not have same number"))
 
-
 // let str = "ilovejavascript"
-// console.log(str.slice(1,5)); // give the limited outputwhat we write in index of string 
-// console.log(str.slice(0,str.length-0)); // give complete string length-0  
-// console.log(str.slice(-6)); // that is puri length me se minus krege kitna kre ga jitni hum value denge yha -1  kre ga matlab ab ye last element ko print kre ga 
+// console.log(str.slice(1,5)); // give the limited outputwhat we write in index of string
+// console.log(str.slice(0,str.length-0)); // give complete string length-0
+// console.log(str.slice(-6)); // that is puri length me se minus krege kitna kre ga jitni hum value denge yha -1  kre ga matlab ab ye last element ko print kre ga
 
 // console.log(str.replace("love", "enjoy")) // replace words
 // console.log(str.replace("o", "x")); // replace only first matching character
@@ -196,17 +195,101 @@
 
 // console.log(fruit.repeat(5))
 
-
-
 // ----------------QS-------
 
-let msg = "    help!";
-let newMsg = msg.trim();
-console.log(newMsg.toUpperCase())
-// console.log(msg.trim(),msg.toUpperCase())
+// let msg = "    help!";
+// let newMsg = msg.trim();
+// console.log(newMsg.toUpperCase())
+// // console.log(msg.trim(),msg.toUpperCase())
 
-let name = "ApnaCollege";
-console.log(name.slice(4,9), name.indexOf("na"), name.replace("Apna","Our"));
+// let name = "ApnaCollege";
+// console.log(name.slice(4,9), name.indexOf("na"), name.replace("Apna","Our"));
 
-let newName = name.slice(4)
-console.log(newName.replaceAll("l","t"))
+// let newName = name.slice(4)
+// console.log(newName.replaceAll("l","t"))
+
+// -------------Arrays-----------------
+
+// let months = ['january','july','march','august'];
+
+// ['july, june', 'march','august']
+
+// console.log(months.shift(),months.shift(),months.unshift('june'),months.unshift('july'));
+// console.log(months)
+
+// let months = ['january','july','march','august'];
+// console.log(months.splice(0, 2, "july", "june"));
+
+// let pLanguages = ['c','c++','html','javascript','java','python','c#','sql'];
+
+// console.log(pLanguages.reverse(),pLanguages.indexOf('javascript'))
+
+// const arr = [1, 2, 3]; //  arr(3)[(1, 2, 3)];
+
+// arr.push(5); //  arr(4)[(1, 2, 3, 5)];
+
+// let nums = [
+//     [1,2],[3,4],[5,6],[7,8]
+// ]
+
+// let gameNums = [
+//     ['X',null,'O'],
+//     [null,'X',null],
+//     ['O',null,'X'],
+
+// ]
+// gameNums[0][1]= 'O'
+// console.log(gameNums[0])
+// console.log(gameNums[1])
+// console.log(gameNums[2])
+
+// ------------QS------------------
+
+// let arr = [7,8,9,10,-132,-23]
+// let n = 5;
+
+// let ans = arr.slice(0,n)
+// console.log(ans)
+//     let nans = arr.slice(n);
+//     console.log(nans)
+
+// let str = prompt("Write any thing");
+
+// if (str.length == 0) {
+//   console.log("String is empty");
+// } else {
+//   console.log(`String is not empty : ${str}`);
+// }
+
+// checking the desire index of this arry charcter is upper case or not
+let char = "HelLo WorLd!";
+let idx = 1;
+if (char[idx] == char[idx].toUpperCase()) {
+  console.log(`Character is in Upper Case ${char[idx]}`);
+} else {
+  console.log(`Character is not in upper case : ${char[idx]}`);
+}
+
+// ------Checking the desire index of this arry is prime or odd
+// let num = [2, 5, 3, 34, 5, 6, 56, 4];
+// let index = 7;
+// if (num[index] % 2 === 0) {
+//   console.log("number is prime");
+// } else {
+//   console.log("num is odd");
+// }
+
+// let string = prompt("enter any string");
+
+// console.log(string);
+
+// console.log(string.trim());
+
+let newArr = [2, 12, 223, 3, 5, "helo", "a"];
+let find = 6;
+
+if (newArr.indexOf(find) != -1) {
+  console.log("element is exist in array");
+} else {
+  console.log("element is not exist in array");
+}
