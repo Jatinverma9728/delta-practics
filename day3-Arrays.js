6d08f42c141d71c80c3134c0962c6a092c394728
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-01-15 20:31:38 +0530

    day 11

---
+++ day3-Arrays.js
@@ -0,0 +1,85 @@
+// -------------Arrays-----------------
+
+// let months = ['january','july','march','august'];
+
+// ['july, june', 'march','august']
+
+// console.log(months.shift(),months.shift(),months.unshift('june'),months.unshift('july'));
+// console.log(months)
+
+// let months = ['january','july','march','august'];
+// console.log(months.splice(0, 2, "july", "june"));
+
+// let pLanguages = ['c','c++','html','javascript','java','python','c#','sql'];
+
+// console.log(pLanguages.reverse(),pLanguages.indexOf('javascript'))
+
+// const arr = [1, 2, 3]; //  arr(3)[(1, 2, 3)];
+
+// arr.push(5); //  arr(4)[(1, 2, 3, 5)];
+
+// let nums = [
+//     [1,2],[3,4],[5,6],[7,8]
+// ]
+
+// let gameNums = [
+//     ['X',null,'O'],
+//     [null,'X',null],
+//     ['O',null,'X'],
+
+// ]
+// gameNums[0][1]= 'O'
+// console.log(gameNums[0])
+// console.log(gameNums[1])
+// console.log(gameNums[2])
+
+// ------------QS------------------
+
+// let arr = [7,8,9,10,-132,-23]
+// let n = 5;
+
+// let ans = arr.slice(0,n)
+// console.log(ans)
+//     let nans = arr.slice(n);
+//     console.log(nans)
+
+// let str = prompt("Write any thing");
+
+// if (str.length == 0) {
+//   console.log("String is empty");
+// } else {
+//   console.log(`String is not empty : ${str}`);
+// }
+
+// checking the desire index of this arry charcter is upper case or not
+// let char = "HelLo WorLd!";
+// let idx = 1;
+// if (char[idx] == char[idx].toUpperCase()) {
+//   console.log(`Character is in Upper Case ${char[idx]}`);
+// } else {
+//   console.log(`Character is not in upper case : ${char[idx]}`);
+// }
+
+// ------Checking the desire index of this arry is prime or odd
+// let num = [2, 5, 3, 34, 5, 6, 56, 4];
+// let index = 7;
+// if (num[index] % 2 === 0) {
+//   console.log("number is prime");
+// } else {
+//   console.log("num is odd");
+// }
+
+// let string = prompt("enter any string");
+
+// console.log(string);
+
+// console.log(string.trim());
+
+// let newArr = [2, 12, 223, 3, 5, "helo", "a"];
+// let find = 6;
+
+// if (newArr.indexOf(find) != -1) {
+//   console.log("element is exist in array");
+// } else {
+//   console.log("element is not exist in array");
+// }
---
+++ day4-loops.js
@@ -0,0 +1,43 @@
+
+// for(let i = 1; i <=500; i++){
+//   console.log(i)
+// }
+// for(let j = 5; j >= 1; j--){
+//   console.log(j)
+// }
+
+// -----print odd numbers 1 to 15
+
+// for (let i = 1; i <= 15; i = i + 2) {
+//   console.log(`number is ${i} is odd`);
+// }
+// for (let i = 100; i >= 1; i = i - 2) {
+//   console.log(`number is ${i} is odd`);
+// }
+
+// for (let i = 2; i <= 10; i = i + 2) {
+//   console.log(i);
+// }
+
+
+// for(let i=5; i<=50; i=i+5){
+//   console.log(i)
+// }
+
+// let n = prompt('Which Number table you want !')
+// n = parseInt(n);
+// for(let i = n; i<=n*10; i = i+n){
+//   console.log(i)
+// }
+
+// for(let i = 1; i<=3; i++){
+//   console.log(`outer loop ${i}`)
+//   for(let j = 1; j<=3;j++){
+//     console.log(j)
+//   }
+// }
+
+
+// ----------While Loop---------------
+
+
