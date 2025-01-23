5509683befbe786539923ff6fa717137440a07dc
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-01-23 20:29:50 +0530

    day 15 function learning

---
+++ day6-Functions.js
@@ -0,0 +1,39 @@
+// let a = 3;
+// let b = 5;
+// function add() {
+//   console.log(a + b);
+// }
+// function sub() {
+//   console.log(a - b);
+// }
+// add();
+// sub();
+
+// function print1to5() {
+//   for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+//   }
+// }
+// print1to5();
+
+// function condition() {
+//   if (a > b) {
+//     console.log("a is bigger");
+//   } else {
+//     console.log("b is bigger");
+//   }
+// }
+// condition();
+
+// function poem(){
+
+//     console.log('Twinkel Twinkel Littel satr')
+//     console.log('how I wonder what you are')
+// }
+// poem();
+
+function dice() {
+  let rand = Math.floor(Math.random() * 6 ) + 1;
+  console.log(rand);
+}
+dice();
