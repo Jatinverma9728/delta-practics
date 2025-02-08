cfdc43ce2cd2aa13da720a81f40f3d7b0500a368
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-02-08 17:24:11 +0530

    7.2

--- day7-timing.js
+++ day7-timing.js
@@ -7,12 +7,12 @@ const student = {
   java: 52,
   cyberSecurity: 71,
   get avg() {
-    console.log(this)
+    console.log(this);
     // let avrage = (linux + java + cyberSecurity) / 3; // iss me anwer nhi milega kyu objct ke ander function  object ki values ko directly acces nhi kr sakta. to krne ke liye kya kr sakte h. to hum 'this' keyword ko use krnge har us cize ke aage jo hum chahiye object ki value.
-//correct is :- 
-let avrage = (this.linux + this.java + this.cyberSecurity) / 3;
+    //correct is :-
+    let avrage = (this.linux + this.java + this.cyberSecurity) / 3;
 
-    console.log(`${this.name} got avrage marks = ${avrage}`)
+    console.log(`${this.name} got avrage marks = ${avrage}`);
   },
 };
 // console.log(student.java)
