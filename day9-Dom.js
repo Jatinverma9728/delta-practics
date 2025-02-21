46c337c64be50e7d046f5a2dccd78173fa634fa2
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-02-21 20:05:51 +0530

    day 9 complete

--- day9-Dom.js
+++ day9-Dom.js
@@ -170,27 +170,32 @@
 
 //Qsl. Create a new input and button element on the page using JavaScript only. Set the text of button to "Click me'
 
-let input = document.createElement("input");
-let btn = document.createElement("button");
-btn.innerText = "Click me";
-let body = document.querySelector("body").append(input, btn);
+// let input = document.createElement("input");
+// let btn = document.createElement("button");
+// btn.innerText = "Click me";
+// let body = document.querySelector("body").append(input, btn);
 
+//Qs2. Add following attributes to the element : Change placeholder value of input to "username" Change the id of button to "btn'
 
-//Qs2. Add following attributes to the element : Change placeholder value of input to "username" Change the id of button to "btn' 
+// input.setAttribute('placeholder','username')
+// btn.setAttribute('id','buttonId')
 
-input.setAttribute('placeholder','username')
-btn.setAttribute('id','buttonId')
+//Qs3. Access the btn using the querySelector and button id. Change the button background color to blue and text color to white.
 
-//Qs3. Access the btn using the querySelector and button id. Change the button background color to blue and text color to white. 
+// let btnid= document.getElementById('buttonId')
+// btnid.style.backgroundColor = "blue";
+// btnid.style.color = "white";
 
-let btnid= document.getElementById('buttonId')
-btnid.style.backgroundColor = "blue";
-btnid.style.color = "white";
+// Qs4. Create an hl element on the page and set its text to "DOM Practice" underlined. Change its color to purple.
 
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "<a>DOM practice</a>";
+// h1.classList.add("color");
+// // h1.style.textDecoration='black wave'
+// document.querySelector("body").append(h1);
 
-// Qs4. Create an hl element on the page and set its text to "DOM Practice" underlined. Change its color to purple. 
+//Qs5. Create a p tag on the page and set its text to "Apna College Delta Practice", where Delta is bold.
 
-let h1= document.createElement('h1');
-h1.innerText='DOM Practice';
-
-body.append(h1)
+// let para = document.createElement("p");
+// para.innerHTML = "Apna College <b>Delta</b> Practice";
+// document.querySelector("body").append(para);
