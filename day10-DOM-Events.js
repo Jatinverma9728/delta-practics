17081c0972adf935991c4f12b394920f94c0b8fc
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-02-24 21:21:29 +0530

    day 10 complete

--- day10-DOM-Events.js
+++ day10-DOM-Events.js
@@ -196,8 +196,22 @@ let btns = document.querySelectorAll("button");
 //   console.log("load is complete");
 // });
 
-let button = document.createElement("button");
-(button.innerText = "click me"), document.body.appendChild(button);
-button.addEventListener("click", function () {
-  button.style.backgroundColor = "green";
-});
+//Qs2. Create a button on the page using JavaScript. Add an event listener to the button that changes the button’s color to green when it is clicked.
+
+// let button = document.createElement("button");
+// (button.innerText = "click me"), document.body.appendChild(button);
+// button.addEventListener("click", function () {
+//   button.style.backgroundColor = "green";
+// });
+
+
+// Qs3. Create an input element on the page with a placeholder ”enter your name” and an H2 heading on the page inside HTML. The purpose of this input element is to enter a user’s name so it should only input letters from a-z, A-Z and space (all other characters should not be detected).Whenever the user inputs their name, their input should be dynamically visible inside the heading.[Please note that no other character apart from the allowed characters should be visible in the heading]
+
+
+
+ document.getElementById("nameInput").addEventListener("input", function () {
+   let filteredValue = this.value.replace(/[^a-zA-Z\s]/g, ""); // this is regex equation that allow only alpabetically and space. Remove non-alphabetic characters
+   this.value = filteredValue; // Update input field to remove invalid characters
+   document.getElementById("displayName").textContent =
+     filteredValue || "Your name will appear here";
+ });
