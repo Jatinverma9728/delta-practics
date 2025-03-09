0c58b1ddba02d4039a93fc6a20bb4eec684005c9
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-03-10 00:33:02 +0530

    day 14 complete and js complete

--- day14-Axio.js
+++ day14-Axio.js
@@ -1,24 +1,72 @@
 // ++++++++++Axios++++++++++
 // it is a libarary to make HTTPS requests. it more better and efficient to send our requests.
 
-let hheading=document.querySelector('h1')
-let btn= document.querySelector('button')
-let url = "https://catfact.ninja/fact";
+// const { log } = require("console");
 
-btn.addEventListener('click', async()=>{
-    let fact = await getFacts()
-    console.log(fact);
-    
-})
+// // random cat facts
+
+// let hheading=document.querySelector('h1')
+// let btn= document.querySelector('button')
+// let url = "https://catfact.ninja/fact";
+
+// btn.addEventListener('click', async()=>{
+//     let fact = await getFacts()
+//     console.log(fact);
+//     let p = document.querySelector('p').innerText=fact
+// })
+
+// async function getFacts() {
+//     try {
+//         let result = await axios.get(url);
+//         return (result.data.fact);
+
+//     } catch (error) {
+//         console.log(error);
+//         return 'no fact found'
+//     }
+// }
+
+// random dog images+++++
+
+// let heading = document.querySelector("h1");
+// let btn2 = document.querySelector("#dog-btn");
+// let url2 = "https://dog.ceo/api/breeds/image/random";
+
+// btn2.addEventListener("click", async () => {
+//   let link = await getImage();
+//   let img = document.querySelector("img");
+// img.setAttribute("src",link)
+
+// });
+
+// async function getImage() {
+//   try {
+//     let result = await axios.get(url2);
+//     return result.data.message;
+//     // console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//     return "no Image found";
+//   }
+// }
+
+// ++++++++=sending headers with axios+++++++++
+
+// basically is use for change the api response formate like html to plain text or json or vice-versa
+
+// const url = "https://icanhazdadjoke.com/";
+
+// async function getJokes() {  
+//   try {
+//     const con = { headers: { Accept: "application/json" } };
+//     let res = await axios.get(url,con);
+//     console.log(res.data.joke);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 
-async function getFacts() {
-    try {
-        let result = await axios.get(url);
-        return (result.data.fact);
-        
-    } catch (error) {
-        console.log(error);
-        return 'no fact found'
-    }
-}
+// +++++++++updating the query Strings with axios++++++++
+
+
