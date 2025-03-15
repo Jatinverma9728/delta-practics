0885a3b77f8fb09d92d590f84dfe93953a53ca67
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-03-15 23:53:59 +0530

    day 15 node js complete almost

--- nodeJs/npm/npm.js
+++ nodeJs/npm/npm.js
@@ -7,13 +7,19 @@
 
 // installing packages npn install 'package name', npm i 'package name'
 
-var figlet = require("figlet");
+// var figlet = require("figlet");
 
-figlet("Jatin verma", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+// figlet("Jatin verma", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+
+// require v/s import we can use import but why?
+// we can't selectively load only the pices we need with require import, we can selectively load only the pices we need, which can save memory.
+
+//loading is synchronous for 'require' but can be asynchronous for 'import'
