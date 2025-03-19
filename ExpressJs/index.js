2a1d27e4991eff9e6a324334baa5ef52ebd49c63
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-03-19 22:52:22 +0530

    express complete

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -8,9 +8,46 @@ app.listen(port, () => {
   console.log(`app is listning on prot ${port}`);
 });
 
-app.use((req, res) => {
-  console.log(req);
-  let code = "<h1>fruits</h1> <ul><li>orange</li><li>apple</li></ul> ";
-  res.send(code);
-  console.log("request recived");
+// app.use((req, res) => {
+//   console.log(req);
+//   let code = "<h1>fruits</h1> <ul><li>orange</li><li>apple</li></ul> ";
+//   res.send(code);
+//   console.log("request recived");
+// });
+
+app.get("/", (req, res) => {
+  res.send("you connected to the route path");
 });
+// app.get("/jatin", (req, res) => {
+//   res.send("you are on jatin path");
+// });
+// app.get("/vaishnavi", (req, res) => {
+//   res.send("you connected vaishnavi path");
+// });
+
+// app.get("*", (req, res) => {
+//   res.send("this path is not valid");
+// });
+// app.post("/", (req, res) => {
+//   res.send("you sent a post request to route");
+// });
+
+// ----------installing nodemon--------------
+
+// to automatically restart with code changes
+
+// -------Querry string-----------
+
+// https://www.google.com/search?q=apple&rlz
+//  in this url there is a after the url there is a search is equal to something. so the search = something is the querry string.
+
+app.get("/search", (req, res) => {
+  let { q } = req.query;
+  if(!q){
+    res.send(`<h1>nothing searched</h1>`)
+  }
+  res.send(`<h1>Search result ${q}</h1>`);
+});
+
+//http://localhost:3000/search?q="apple"
+//http://localhost:3000/search?q="apple"&"red"
