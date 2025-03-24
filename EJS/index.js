b517cb0944206a5fa71b6aa614c3db22d5fdc711
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-03-24 23:21:07 +0530

    ejs complete

--- EJS/index.js
+++ EJS/index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 const port = 3000;
 
 app.set("view engine", "ejs");
@@ -13,13 +13,30 @@ app.get("/hello", (req, res) => {
   res.send("this is hello");
 });
 app.get("/", (req, res) => {
-  res.render("home.ejs"); // .ejs extension is optional
+  res.render("hello"); // .ejs extension is optional
+});
+app.get("/rolldice", (req, res) => {
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rollDice.ejs", {
+    num: diceVal,
+  }); // .ejs extension is optional
+});
+
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  const instaData = require("./data.json");
+  // console.log(instaData)
+  const data = instaData[username];
+  if (data) {
+    res.render("insta.ejs", { data });
+  } else {
+    res.render("error.ejs");
+  }
 });
 
 app.listen(port, () => {
   console.log("listning on port", port);
 });
 
-
 //-----interpolation syntax--------
-//Interpolation syntax is a way to insert variables or expressions dynamically to the markup text 
+//Interpolation syntax is a way to insert variables or expressions dynamically to the markup text
