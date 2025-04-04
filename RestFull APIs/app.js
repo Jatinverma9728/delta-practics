36af7bd87c887bcf0f2db5a1d6be16614bfc6ce4
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-04-04 23:27:01 +0530

    half complete restfull apis

--- RestFull APIs/app.js
+++
@@ -1,5 +0,0 @@
-// complete discription of restfull APIs in draw.tldr 
-
-// we are make a basic project like quora post (like twitter)
-
-//
---
+++ RestFull-APIs/index.js
@@ -0,0 +1,58 @@
+// complete discription of restfull APIs in draw.tldr
+
+// we are make a basic project like quora post (like twitter)
+
+// CRUD (Creat, Read, Update, Delete)
+
+const express = require("express");
+const { log } = require("node:console");
+const app = express();
+const port = 3000;
+const path = require("path");
+app.set("view engin", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+let posts = [
+  {
+    id: "1a",
+    username: "jatinverma",
+    content: "hello this is jatin",
+  },
+  {
+    id: "1b",
+    username: "vaishnavi",
+    content: "hello this is vaishnavi",
+  },
+  {
+    id: "1c",
+    username: "verma",
+    content: "hello this is verma",
+  },
+];
+
+app.listen(port, () => {
+  console.log(`Listning to the port: ${port}`);
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+ res.render('show.ejs',{post})
+//   res.send("request is working");
+});
