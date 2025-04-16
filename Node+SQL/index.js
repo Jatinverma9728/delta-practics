080f6faea5b6e6b51fe8c61b342c048fc4353257
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-04-16 23:10:21 +0530

    sql connecting with node and acessing the database through node cli, my sql package, workbench, using files

---
+++ Node+SQL/index.js
@@ -0,0 +1,29 @@
+const { faker, th } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "delta_app",
+  password: "Jatinkumar@123",
+});
+
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+connection.end();
+let getRandomUser = () => {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.username(), // before version 9.1.0, use userName()
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+};
+
+// console.log(getRandomUser());
