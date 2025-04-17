ffda33ef4eec900c64e75a49bf139de22dde327e
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-04-17 23:12:12 +0530

    ading data to sql database through node js

--- Node+SQL/index.js
+++ Node+SQL/index.js
@@ -1,29 +1,45 @@
 const { faker, th } = require("@faker-js/faker");
 const mysql = require("mysql2");
 
+// connection with mySQL
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "delta_app",
   password: "Jatinkumar@123",
 });
+// gitting some fake data for adding in database for exercise
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+// querry inserting new data
+let q = "INSERT INTO usr (id,username,email,password) VALUES ?";
+
+// manually create data
+// let users = [
+//   ["1263", "123_neuser", "abcg@gmail.com", "abgc"],
+//   ["14", "124_newuser", "acd@gmail.com", "abd"],
+// ];
+
+let data = [];
+for (let i = 0; i <= 100; i++) {
+  data.push(getRandomUser()); //100 fake user data
+}
 
 try {
-  connection.query("SHOW TABLES", (err, result) => {
+  connection.query(q, [data], (err, result) => {
     if (err) throw err;
     console.log(result);
   });
 } catch (err) {
   console.log(err);
 }
-connection.end();
-let getRandomUser = () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-};
 
-// console.log(getRandomUser());
+// need to end the connection if we not end this it run infinite
+connection.end();
--- Node+SQL/schema.sql
+++ Node+SQL/schema.sql
@@ -1,1 +1,10 @@
 show tables;
+
+CREATE TABLE usr (
+    id VARCHAR(50) PRIMARY KEY,
+    username VARCHER(50) UNIQUE,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
+
+
+);
