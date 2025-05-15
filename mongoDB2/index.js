baa81de07eba3c1cc2dd152e7c7b45aed6f779a3
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-05-15 23:54:25 +0530

    mongoose 3/4 complete

--- mongoDB2/index.js
+++ mongoDB2/index.js
@@ -26,6 +26,48 @@ const userSchema = new mongoose.Schema({
 // const User = mongoose.model("User", userSchema); //users
 const Employee = mongoose.model("Employee", userSchema); //users
 
+// inserting data
 
-// inserting data 
+const User = mongoose.model("User", userSchema);
 
+const user1 = new User({
+  name: "Jatin",
+  email: "Jatinverma9728@gmail.com",
+  age: 22,
+});
+const user2 = new User({
+  name: "vaishnavi",
+  email: "vaishnavisharma9123@gmail.com",
+  age: 20,
+});
+
+// saving this data to mongo db
+
+user1.save();
+user2
+  .save()
+  .then((res) => {
+    console.log("sucessfully data add to the db", res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// insterting multiple data
+
+User.insertMany([
+  { name: "JatinVerma", email: "vermajatin477@gmail.com", age: 22 },
+  { name: "Vaishnavi", email: "Vaishnavi@gmail.com", age: 22 },
+  { name: "verma", email: "verma@gmail.com", age: 22 },
+  { name: "bulbul", email: "bulbul@gmail.com", age: 22 },
+]).then((data) => {
+  console.log(data);
+});
+
+
+// NOTE:  mongoose use operation buffering
+
+// mongoose lets you start using your models immediately, without waiting for mongoose to establish a connection to mongodb
+
+
+// Find in mongoose
