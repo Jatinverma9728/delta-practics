7e2ce15a8968a134c8016e5b5071fd21699cfa7c
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-05-21 23:09:04 +0530

    almost complete mongoose

--- mongoDB2/index.js
+++ mongoDB2/index.js
@@ -24,50 +24,62 @@ const userSchema = new mongoose.Schema({
 //Models in mongoose is a class with which we construct documents.
 
 // const User = mongoose.model("User", userSchema); //users
-const Employee = mongoose.model("Employee", userSchema); //users
+// const Employee = mongoose.model("Employee", userSchema); //users
 
 // inserting data
 
 const User = mongoose.model("User", userSchema);
 
-const user1 = new User({
-  name: "Jatin",
-  email: "Jatinverma9728@gmail.com",
-  age: 22,
-});
-const user2 = new User({
-  name: "vaishnavi",
-  email: "vaishnavisharma9123@gmail.com",
-  age: 20,
-});
+// const user1 = new User({
+//   name: "Jatin",
+//   email: "Jatinverma9728@gmail.com",
+//   age: 22,
+// });
+// const user2 = new User({
+//   name: "vaishnavi",
+//   email: "vaishnavisharma9123@gmail.com",
+//   age: 20,
+// });
 
 // saving this data to mongo db
 
-user1.save();
-user2
-  .save()
-  .then((res) => {
-    console.log("sucessfully data add to the db", res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// user1.save();
+// user2
+//   .save()
+//   .then((res) => {
+//     console.log("sucessfully data add to the db", res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // insterting multiple data
 
-User.insertMany([
-  { name: "JatinVerma", email: "vermajatin477@gmail.com", age: 22 },
-  { name: "Vaishnavi", email: "Vaishnavi@gmail.com", age: 22 },
-  { name: "verma", email: "verma@gmail.com", age: 22 },
-  { name: "bulbul", email: "bulbul@gmail.com", age: 22 },
-]).then((data) => {
-  console.log(data);
-});
-
+// User.insertMany([
+//   { name: "JatinVerma", email: "vermajatin477@gmail.com", age: 22 },
+//   { name: "Vaishnavi", email: "Vaishnavi@gmail.com", age: 22 },
+//   { name: "verma", email: "verma@gmail.com", age: 22 },
+//   { name: "bulbul", email: "bulbul@gmail.com", age: 22 },
+// ]).then((data) => {
+//   console.log(data);
+// });
 
 // NOTE:  mongoose use operation buffering
 
 // mongoose lets you start using your models immediately, without waiting for mongoose to establish a connection to mongodb
 
-
 // Find in mongoose
+
+//model.find() return a thennable object
+// mongoose Queries are not promises.but they have a .then()
+
+User.findById("68262ce5742d539411be8ef8")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// model.findOne() returns a single result
